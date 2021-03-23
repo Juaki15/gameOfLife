dc625573615674b2231178c065666138089eb989
juaki1502@gmail.com <juaki1502@gmail.com>
2021-03-23 13:50:34 +0100

    añadiendo matriz

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,7 +1,9 @@
 package aplicacion;
+import dominio.Tablero;
 
 public class Principal {
 	public static void main (String args[]){
-
+		Tablero tablero = new Tablero();
+		tablero.generarEstadoActual();
 	}
 }
--- src/dominio/Tablero.java
+++ src/dominio/Tablero.java
@@ -8,29 +8,16 @@ import java.io.IOException;
 
 public class Tablero{
 	private static int DIMENSION  = 30;
-	private int estadoActual[][] = new int [DIMENSION][DIMENSION];
-	private int estadoSiguiente[][] = new int [DIMENSION][DIMENSION]; 
-
-	public void generarEstadoActual(){
-		int elementoArray = 0;
-		File fichero = new File("matriz.txt");
-		Scanner s = new Scanner(fichero);
-		FileWriter fw = new FileWriter("matriz.txt");
-		BufferedWriter bw = new BufferedWriter(fw);
-		try{
-			for (int i = 0; i < estadoActual.length; i++){
-				for (int j = 0; j < estadoActual.length; j++){
-					estadoActual[i][j] = ((int) Math.random() * 1);
-					bw.write(estadoActual[i][j]);
-					bw.flush();
-				}
-			bw.newLine();	
-			}	
-		}catch(IOException | InterruptedException e) {
-			System.out.println("Error");
-		}	
-		
-	}
+	private int[][] estadoActual = new int [DIMENSION][DIMENSION];
+	private int[][] estadoSiguiente = new int [DIMENSION][DIMENSION]; 
 	
-	//public void transitarAlEstadosiguiente	
+	public void generarEstadoActual(){
+    		for (int i = 0 ; i < estadoActual.length; i++) {
+			for (int j = 0; j < estadoActual[i].length; j++) {
+				int matriz = estadoActual[i][j] = ((int) Math.random()*1);
+				//System.out.println(matriz);
+			}	
+		}
+		System.out.println(estadoActual);
+	}
 }
